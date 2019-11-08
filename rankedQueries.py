455ccccdc9cfa6eed8216f60790bf6d8f21e651c
ClementGuittat <32964240+ClementGuittat@users.noreply.github.com>
2019-11-08 12:06:46 +0100

    prendre en compte seulement les fichier .py où il y a du code avec début invertedFile et ranked

---
+++ rankedQueries.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  8 11:28:26 2019
+
+@author: clementguittat
+"""
+
+from collections import Counter
+from globals import invertedFile as IF
+
+def naiveAlgo(query):
+    finalDic = dict();
+    for keyword in query.split():
+        if (keyword in IF):
+            finalDic = dict(Counter(finalDic) + Counter(IF[keyword]))
+    print(finalDic)
+    listValues = list(finalDic.values())
+    listKeys = list(finalDic.keys())
+    bestDoc = listKeys[listValues.index(max(listValues))]
+    print(bestDoc)
+    
+if __name__ == "__main__":
+    IF = {"you": {1: 3, 2: 2}, "are": {1: 2}, "tuples": {2: 2}}
+    naiveAlgo("you tuples")
+    

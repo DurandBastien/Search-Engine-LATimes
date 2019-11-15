437e6ede92868adcfac06ca9b8ec149c170ff02c
ClementGuittat <32964240+ClementGuittat@users.noreply.github.com>
2019-11-15 09:28:27 +0100

    add ranking et commentaires naiveAlgo

--- SearchAlgorithms/searchAlgorithms.py
+++ SearchAlgorithms/searchAlgorithms.py
@@ -6,21 +6,33 @@ Created on Fri Nov  8 11:28:26 2019
 @author: clementguittat
 """
 
+import sys
+sys.path.insert(1, '/Users/clementguittat/Documents/INSA LYON/5A/QueryText/Search-Engine-LATimes')
 from collections import Counter
 from Globals.globals import invertedFile as IF
 
+# Algo naif 
+# Fonctionnement pour 1 mot-clé dans la requête => 
+# on calcule le nombre d'occurences dans chacun des documents. Le score associé à chacun des documents correspond à ce nb d'occurences
+# puis je classe les documents par rapport à leur score  
+
+#Fonctionnement pour plusieurs mot-clé dans la requête =>
+# pour chaque mot-clé on calcule le score associé au document. Puis pour chaque document, on calcule son score global sur la requête en additionnant le score obtenu par mot-clé
+# puis je classe les documents par rapport à leur score 
 def naiveAlgo(query):
     if(not IF):
         return []
     finalDic = dict();
     for keyword in query.split():
         if (keyword in IF):
-            finalDic = dict(Counter(finalDic) + Counter(IF[keyword]))
+            finalDic = dict(Counter(finalDic) + Counter(IF[keyword])) # additionne les valeurs des deux dictionnaires avec la même clé
     print(finalDic)
-    listValues = list(finalDic.values())
-    listKeys = list(finalDic.keys())
-    bestDoc = listKeys[listValues.index(max(listValues))]
-    print(bestDoc)
+    ranking(finalDic)
+
+# 
+def ranking(finalDic):
+    for docId, freq  in sorted(finalDic.items(), key=lambda x: x[1], reverse=True)[:10]:
+        print("document ID:", docId, " freq:", freq)
     
 if __name__ == "__main__":
     IF = {"you": {1: 3, 2: 2}, "are": {1: 2}, "tuples": {2: 2}}
